ed4720f3cc0389f5ad5c8715f3c2a1d7b12abd9b
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-05-11 18:56:21 -0500

    added goat data

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.scss';
 
+import goatData from '../helpers/data/goatData';
+
 class App extends React.Component {
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    console.error('goats', goats);
+  }
+
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">CRAP</button>
+        <h2 className='goat-title'>Goats</h2>
+        <button className="btn btn-success"><i className="fas fa-tree fa-3x"></i></button>
       </div>
     );
   }
